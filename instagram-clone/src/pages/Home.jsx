3904222811f61872f8215b9f9bde18ff31e5448c
Eldana Kibru <156713119+eldianak21@users.noreply.github.com>
2024-12-23 00:56:08 +0300

    home and profile page pages added

---
+++ instagram-clone/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Feed from '../components/Feed';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Feed />
+    </div>
+  );
+};
+
+export default Home;
